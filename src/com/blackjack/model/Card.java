27235f607d60ad5618d7bc3638c0e65828ccd751
Lakea <lakeag@tlgcohort.com>
2024-07-29 13:41:12 -0400

    Added the remainder of the spades card collection totaling 13. Ace-King

--- src/com/blackjack/model/Card.java
+++ src/com/blackjack/model/Card.java
@@ -4,9 +4,20 @@ public enum Card {
     ACE_SPADES,
     TWO_SPADES,
     THREE_SPADES,
+    FOUR_SPADES,
+    FIVE_SPADES,
+    SIX_SPADES,
+    SEVEN_SPADES,
+    EIGHT_SPADES,
+    NINE_SPADES,
+    TEN_SPADES,
+    JACK_SPADES,
+    QUEEN_SPADES,
+    KING_SPADES,
 
-    // bob's work here
+
+    // Bob's work here
     ACE_CLUBS,
     TWO_CLUBS,
-    THREE_CLUBS;
+    THREE_CLUBS,;
 }
